49ed2b56fd693f6388aa8cf3e96b219ea48474c1
Damiano Papi <dpapi68@gmail.com>
2020-04-30 19:44:14 +0200

    [FIX] Product notification

--- src/classes/Discord.js
+++ src/classes/Discord.js
@@ -5,11 +5,12 @@ const hook = new Webhook(global.config.webhook_url);
 let Discord = {};
 
 Discord.notifyProduct = async ({title, sellerUrl, image, url, variants}) => {
-    if(variants.length == 0)
+    var availablesVariants = variants.filter(x => x.available);
+    if(availablesVariants.length == 0)
         return;
 
     const embed = new MessageBuilder().setTitle(title).setAuthor(sellerUrl, image, url).setURL(url)
-    .addField('Sizes', variants.map(x => x.title).join('\n'), true).addField('Price', variants[0].price, true).setImage(image); 
+    .addField('Sizes', availablesVariants.map(x => x.title).join('\n'), true).addField('Price', variants[0].price, true).setImage(image); 
 
     hook.send(embed);
 }
