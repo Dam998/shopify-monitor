4e3f6732c6f73597dda6cf622d26eae55e1822b6
Damiano Papi <dpapi68@gmail.com>
2020-05-01 17:29:07 +0200

    [FIX] Discord api not support very long fields

--- src/classes/Discord.js
+++ src/classes/Discord.js
@@ -9,30 +9,29 @@ Discord.notifyProduct = async ({title, sellerUrl, image, url, variants}) => {
     var availablesVariants = variants.filter(x => x.available);
     if(availablesVariants.length == 0)
         return;
-        
-    var sizesDescription = ""
-    var truncate = false;
-    var sizesDescription2 = ""
-    availablesVariants.forEach(variant => {
-        var toAdd = `${variant.title} [[ATC](https://${sellerUrl}/cart/add.js?id=${variant.id})]\n`
-        if(truncate || sizesDescription.length + toAdd.length > 1024) {
-            truncate = true;
-            sizesDescription2 += toAdd;
-        }
-        else {
-            sizesDescription += toAdd;
-        }
-    });
 
-    const embed = new MessageBuilder().setTitle(title).setAuthor(sellerUrl, image, url).setURL(url)
-    .addField('Sizes', sizesDescription, true)
-    
-    if(truncate){
-        embed.addField('Sizes', sizesDescription2, true)
-    }
+    var sizesDescription = []
+    sizesDescription.push("")
+    var count = 0
 
-    embed.addField('Price', variants[0].price, true)
-    .addField('Links', `[[Cart](https://${sellerUrl}/cart)]`, true).setThumbnail(image); 
+    availablesVariants.forEach(x => {
+        var toAdd = `${x.title} [[ATC](https://${sellerUrl}/cart/add.js?id=${x.id})]\n`
+        if(sizesDescription[count].length + toAdd.length > 1024){
+            sizesDescription.push(toAdd);
+            count++;
+        }
+        else{
+            sizesDescription[count] += toAdd;
+        }
+    })
+
+    const embed = new MessageBuilder().setTitle(title).setAuthor(sellerUrl, image, url).setURL(url);
+
+    sizesDescription.forEach(x => {
+        embed.addField('Sizes', x, true)
+    })
+
+    embed.addField('Price', variants[0].price, true).addField('Links', `[[Cart](https://${sellerUrl}/cart)]`, true).setThumbnail(image); 
 
     hook.send(embed);
 }
